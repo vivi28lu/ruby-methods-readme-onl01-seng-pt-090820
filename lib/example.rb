ef27d22d390217aee1baac2a8dde5ffb4bb5078f
Viviana Lugo <vivi28lu@gmail.com>
2020-09-22 02:59:17 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,8 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+def greeting (name, language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer"
+end
+greeting ("Sophie", "Ember.js")
+
